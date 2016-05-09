f43e4de5d19406f48643bd9764bf36e4642e7af4
Trebuh <trebuh@users.noreply.github.com>
2016-05-10 01:04:49 +0200

    Added config.py

---
+++ online_watcher/config.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+log = {
+    'line_format': '[%(asctime)s][%(levelname)s] %(message)s'
+    'file_path': '/var/log/online_notifier.log'
+    'file_size': '1000000'
+    'level': logging.DEBUG
+}
+
+watcher_params = {
+    'parsed_url': 'https://console.online.net/fr/order/server'
+    'watched_servers': ['Dedibox XC SATA 2016', 'Dedibox XC SSD 2015']
+    'sleep_time': 300
+}
+
+request_header = {
+    'User-agent': 'Mozilla/5.0'
+}
+
+sms_params = {
+    'sms_url': 'https://smsapi.free-mobile.fr/sendmsg'
+    'payload': {
+        'user': '123456'
+        'pass': 'ABCDEF'
+        'msg': ''
+    }
+}
