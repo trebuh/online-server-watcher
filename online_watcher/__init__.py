50a3c8eef7244df7c20c3d859db84a868c32327a
Trebuh <trebuh@users.noreply.github.com>
2016-05-13 11:41:26 +0200

    Updated info in init

---
+++ online_watcher/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    This program monitors the availability of a given list of servers
+    provided by Online. When a watched server is available, it sends a text
+    using the API provided by Free (French mobile provider)
+"""
+__title__ = 'online_watcher'
+__version__ = '0.0.1'
+__author__ = 'Trebuh'
+__repo__ = 'https://github.com/trebuh/online-server-watcher'
+__license__ = 'MIT  License'
+
